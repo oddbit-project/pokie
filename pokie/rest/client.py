31f746f3cfcf6e8a8c657b32c1d76cdf3eb70c00
João Pinheiro <aforwardslash@gmail.com>
2024-06-25 01:45:10 +0100

    api client

--- pokie/rest/client.py
+++ pokie/rest/client.py
@@ -1,8 +1,10 @@
 import json
+import requests
+from requests import Response
 
 
 class RestResponse:
-    def __init__(self, response):
+    def __init__(self, response:Response):
         self.success = True
         self.error = None
         self.data = None
@@ -16,9 +18,8 @@ class RestResponse:
         self.code = response.status_code
         self.success = response.status_code == 200
 
-        if response.data:
-            data = json.loads(response.data)
-
+        data = response.json()
+        if data:
             if "data" in data.keys():
                 self.data = data["data"]
 
@@ -30,41 +31,53 @@ class RestResponse:
                     self.form_error = self.error["formError"]
 
 
+class RestAuthInterface:
+
+    def authenticate(self, client) -> bool:
+        pass
+
 class RestClient:
-    def __init__(self, client, base_url="/v1"):
-        self.client = client
+    def __init__(self, base_url="",auth:RestAuthInterface=None, headers:dict = None):
+        self.session = requests.Session()
         self.base_url = base_url
-        self.headers = {"Content-type": "application/json"}
+        if not headers:
+            headers = {"Content-type": "application/json"}
+        self.headers = headers
+        self.authenticated = auth is None
+        if not self.authenticated:
+            if not auth.authenticate(self):
+                raise RuntimeError("REST authentication failed")
+            self.authenticated = True
 
     def post(self, url: str, data=None) -> RestResponse:
         return RestResponse(
-            self.client.post(
+            self.session.post(
                 "{}{}".format(self.base_url, url), json=data, headers=self.headers
             )
         )
 
     def post_formdata(self, url: str, data=None, files=None) -> RestResponse:
         return RestResponse(
-            self.client.post("{}{}".format(self.base_url, url), data=data, files=files)
+            self.session.post("{}{}".format(self.base_url, url), data=data, files=files)
         )
 
     def get(self, url: str, data=None) -> RestResponse:
         return RestResponse(
-            self.client.get(
+            self.session.get(
                 "{}{}".format(self.base_url, url), json=data, headers=self.headers
             )
         )
 
     def put(self, url: str, data=None) -> RestResponse:
         return RestResponse(
-            self.client.put(
+            self.session.put(
                 "{}{}".format(self.base_url, url), json=data, headers=self.headers
             )
         )
 
     def delete(self, url: str, data=None) -> RestResponse:
         return RestResponse(
-            self.client.delete(
+            self.session.delete(
                 "{}{}".format(self.base_url, url), json=data, headers=self.headers
             )
         )
