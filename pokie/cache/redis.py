e77d63b27b5a33639cc80e6b734da1dfe3d3e5f3
jpinheiro <jpinheiro@blackshield.pt>
2025-11-07 16:16:06 +0000

    redis client refactor

--- pokie/cache/redis.py
+++ pokie/cache/redis.py
@@ -9,11 +9,7 @@ from pokie.constants import DI_REDIS
 
 class RedisCache(BaseRedisCache, Injectable):
     def __init__(self, di: Di):
-        # base class init is completely overridden
         if not di.has(DI_REDIS):
             raise RuntimeError("DI_REDIS not found; maybe RedisFactory is missing?")
         self.set_di(di)
-        self._serialize = pickle.dumps
-        self._deserialize = pickle.loads
-        self._prefix = None
-        self._redis = di.get(DI_REDIS)
+        super().__init__(backend=di.get(DI_REDIS))
